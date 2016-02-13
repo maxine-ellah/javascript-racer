495b882b89c05974951691e4dcf05acd13ca85fd
Maxine Ellah <maxine.ellah@gmail.com>
2016-02-13 19:43:11 +1300

    add winner alerts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var player1 = 81;
 var player2 = 80;
+var score1 = 0;
+var score2 = 0;
+var trackLength = 9;
 
 var racerTable1 = document.getElementsByClassName('racer_table1')[0];
 var racerTable2 = document.getElementsByClassName('racer_table2')[0];
@@ -8,14 +11,21 @@ document.onkeyup = function(e) {
   switch (e.keyCode) {
     case player1:
       var cells = racerTable1.getElementsByTagName('td');
+      if (score1 === trackLength) {
+      alert("Player one wins!!!");
+      }
       for (var i=0; i < cells.length; i++) {
-        if (cells[i].className.indexOf('active') !== -1) {
-          if (i === (cells.length - 1)) {
+          if (cells[i].className.indexOf('active') !== -1) {
+            if (i === (cells.length - 1)) {
             // do nothing, all the way left
           } else {
             cells[i].className = '';
             cells[i+1].className = 'active';
+            if (score1 < trackLength) {
+            score1 ++;
+            console.log(score1) }
             break;
+          
           }
         }
       }
@@ -23,6 +33,9 @@ document.onkeyup = function(e) {
     
     case player2:
       var cells = racerTable2.getElementsByTagName('td');
+      if (score2 === trackLength) {
+      alert("Player two wins!!!");
+      }
       for (var i=0; i < cells.length; i++) {
         if (cells[i].className.indexOf('active') !== -1) {
           if (i === (cells.length - 1)) {
@@ -30,10 +43,13 @@ document.onkeyup = function(e) {
           } else {
             cells[i].className = '';
             cells[i+1].className = 'active';
+            if (score2 < trackLength) {
+            score2 ++;
+            console.log(score2) }
             break;
           }
         }
       }
       break;
+      }
   }
-}
