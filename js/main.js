96c0c098c27fc18f8465ed694e62f5bfa00821b9
Maxine Ellah <maxine.ellah@gmail.com>
2016-02-14 12:45:34 +1300

    add racer icons

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var player1 = 81;
 var player2 = 80;
 var score1 = 0;
 var score2 = 0;
-var trackLength = 9;
+var trackLength = 10;
 
 var racerTable1 = document.getElementsByClassName('racer_table1')[0];
 var racerTable2 = document.getElementsByClassName('racer_table2')[0];
@@ -11,10 +11,9 @@ document.onkeyup = function(e) {
   switch (e.keyCode) {
     case player1:
       var cells = racerTable1.getElementsByTagName('td');
-      if (score1 === trackLength) {
-      alert("Player one wins!!!");
-      location.reload()
-      }
+      if (score1 < trackLength) {
+            score1 ++;
+            console.log(score1) }
       for (var i=0; i < cells.length; i++) {
           if (cells[i].className.indexOf('active') !== -1) {
             if (i === (cells.length - 1)) {
@@ -22,9 +21,10 @@ document.onkeyup = function(e) {
           } else {
             cells[i].className = '';
             cells[i+1].className = 'active';
-            if (score1 < trackLength) {
-            score1 ++;
-            console.log(score1) }
+            if (score1 === trackLength) {
+            alert("Player one wins!!!");
+            location.reload()
+            }
             break;
           
           }
@@ -34,10 +34,9 @@ document.onkeyup = function(e) {
     
     case player2:
       var cells = racerTable2.getElementsByTagName('td');
-      if (score2 === trackLength) {
-      alert("Player two wins!!!");
-      location.reload()
-      }
+      if (score2 < trackLength) {
+            score2 ++;
+            console.log(score2) }
       for (var i=0; i < cells.length; i++) {
         if (cells[i].className.indexOf('active') !== -1) {
           if (i === (cells.length - 1)) {
@@ -45,9 +44,12 @@ document.onkeyup = function(e) {
           } else {
             cells[i].className = '';
             cells[i+1].className = 'active';
-            if (score2 < trackLength) {
-            score2 ++;
-            console.log(score2) }
+            
+
+            if (score2 === trackLength) {
+            alert("Player two wins!!!");
+            location.reload()
+            }
             break;
           }
         }
